70330038d3c2268ee92e3becaac3c0e0829733ff
Ashish Sadhuka <ashish852004@gmail.com>
2025-06-28 09:27:28 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "ashishtech85"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
